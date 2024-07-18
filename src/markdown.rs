3592a1ddd78211721f53b020f710765c08b5bb92
Tnixc <tnixxc@gmail.com>
2024-07-18 20:09:54 +0800

    add math

--- src/markdown.rs
+++ src/markdown.rs
@@ -6,8 +6,16 @@ use fancy_regex::Regex;
 
 const MARKDOWN_ELEMENT_PATTERN: &str = r#"(?<!<!--)<markdown>([\s\S]+?)<\/markdown>(?!-->)"#;
 pub fn markdown_element(mut string: String) -> String {
-    let codefence_syntax_highlighter = SyntectAdapterBuilder::new();
+    let codefence_syntax_highlighter = SyntectAdapterBuilder::new().theme("Solarized (light)");
     let mut plugins = Plugins::default();
+    let mut options = Options::default();
+    options.extension.math_code = true;
+    options.extension.math_dollars = true;
+    options.extension.superscript = true;
+    options.extension.autolink = true;
+    options.extension.table = true;
+    options.extension.tasklist = true;
+
     let built = &codefence_syntax_highlighter.build();
     plugins.render.codefence_syntax_highlighter = Some(built);
 
@@ -17,16 +25,11 @@ pub fn markdown_element(mut string: String) -> String {
     for f in re_markdown.find_iter(&string.to_owned()) {
         if f.is_ok() {
             let found = f.unwrap().as_str();
-            let amount = found
-                .trim_start_matches("<markdown>")
-                .lines()
-                .skip(1)
-                .next();
             let res = found
                 .trim_start_matches("<markdown>")
                 .trim_end_matches("</markdown>");
 
-            let rendered = &markdown_to_html_with_plugins(&res, &Options::default(), &plugins);
+            let rendered = &markdown_to_html_with_plugins(&res, &options, &plugins);
             string = string.replacen(found, rendered, 1);
         }
     }
