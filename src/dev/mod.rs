00d26c6f8d4f25b056430dea4d6fb20ce083c05c
Tnixc <tnixxc@gmail.com>
2024-08-25 20:02:27 +0800

    get websockets reloading to work

--- src/dev/mod.rs
+++ src/dev/mod.rs
@@ -2,13 +2,16 @@ use crate::error::{ErrorType, ProcessError, WithItem};
 use crate::*;
 use color_print::{cformat, cprintln};
 use notify::{RecursiveMode, Watcher};
-use rouille::websocket;
 use rouille::Response;
+use serde_json;
+use simple_websockets::{Event, Message, Responder};
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Receiver;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use WithItem::None;
@@ -31,35 +34,53 @@ fn dev_rebuild(res: Result<notify::Event, notify::Error>) -> Result<(), ProcessE
         }
     }
 }
-
-fn spawn_websocket_handler_old(_receiver: Receiver<String>) -> () {
-    thread::spawn(move || {
-        println!("");
-        cprintln!("<g>websocket for reloading is listening on port 2727</>");
-        rouille::start_server("localhost:2727", move |req| {
-            let (response, websocket) = rouille::try_or_400!(websocket::start(req, Some("echo")));
-            println!("{:?},{:?}", response, websocket);
-            let mut ws = websocket.recv().unwrap();
-            for _ in 0..10 {
-                let r = ws.send_text("String");
-                println!("{:?}", r);
-            }
-            return response;
-        })
-    });
-}
-
 fn spawn_websocket_handler(receiver: Receiver<String>) -> () {
-    thread::spawn(move || {
-        println!("");
-        println!("WebSocket for reloading is listening on port 2727");
+    let clients: Arc<Mutex<HashMap<u64, Responder>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    let event_hub = simple_websockets::launch(2727).expect("failed to listen on port 2727");
+
+    let clients_clone = Arc::clone(&clients);
+    thread::spawn(move || loop {
+        let message = receiver.recv().unwrap();
+
+        if message == "reload" {
+            let locked_clients = clients_clone.lock().unwrap();
+            for (_, responder) in locked_clients.iter() {
+                let json = serde_json::json!(
+                    {
+                        "action": "reload"
+                    }
+                );
+                let signal = Message::Text(json.to_string());
+                responder.send(signal);
+            }
+        }
     });
+
+    loop {
+        match event_hub.poll_event() {
+            Event::Connect(client_id, responder) => {
+                println!("A client connected with id #{}", client_id);
+                let mut locked_clients = clients.lock().unwrap();
+                locked_clients.insert(client_id, responder);
+            }
+            Event::Disconnect(client_id) => {
+                println!("Client #{} disconnected.", client_id);
+                let mut locked_clients = clients.lock().unwrap();
+                locked_clients.remove(&client_id);
+            }
+            _ => {}
+        }
+    }
 }
 
 pub fn spawn_watcher(args: Vec<String>) -> () {
     cprintln!("<k!>|-----------------------------------|</>");
     cprintln!("| <s>Now serving <y><u>http://localhost:7272</></></> |");
     cprintln!("<k!>|-----------------------------------|</>");
+
+    let (sender, receiver) = channel::<String>();
+    thread::spawn(|| spawn_websocket_handler(receiver));
 
     let _ = build(args.clone(), true).map_err(|e| {
         eprintln!("{}", cformat!("  <k>|</> <s><r>Build error</></>: {e}"));
@@ -74,9 +95,6 @@ pub fn spawn_watcher(args: Vec<String>) -> () {
     let config = notify::Config::default()
         .with_compare_contents(true)
         .with_poll_interval(Duration::from_millis(200));
-
-    let (sender, receiver) = channel::<String>();
-    spawn_websocket_handler(receiver);
 
     let mut watcher = notify::PollWatcher::new(
         move |res| {
